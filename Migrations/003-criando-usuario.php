65660f7df97030fc0e7f5c6a242ee1d6adaac6cd
Lucas <lucaspicoli.leme@gmail.com>
2025-01-10 17:22:28 -0300

    Cria conexão com banco
    Criado arquivo config.php conectanco com o banco
    Insere usuario administrador no banco e salva migrations

---
+++ Migrations/003-criando-usuario.php
@@ -0,0 +1,5 @@
+/*
+
+INSERT INTO usuarios (nome, email, senha, tipo_usuario) VALUES ('lucas', 'lucas@gmail.com', '123', 'adm');
+
+*/
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUser = 'lucas';
+$dbSenha = 'Luc@s1995';
+$dbName = 'concessionaria';
+
+$conexao = new mysqli($dbHost,$dbUser,$dbSenha,$dbName);
+
+/*
+if($conexao->connect_errno){
+    echo"Erro";
+
+}else{
+    echo"Conetado";
+}
+*/
+
+?>
