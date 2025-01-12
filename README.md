a4e9bdcc882cbc260bb685d9c846718c07ec4184
Lucas <lucaspicoli.leme@gmail.com>
2025-01-12 17:23:23 -0300

    Remove alguns include once e edição de arquivo README

--- README.md
+++ README.md
@@ -5,4 +5,11 @@
 Criando banco de dados
 - Criando tabela de usuários;
 - Criando tabela de carros;
-- Criando tabela de comissão; 
+- Criando tabela de comissão;
+
+Criando Front com páginas para o usuário administrativo
+
+Criando Back para painel administravito
+- Conexão com o Banco de Dados
+- Iniciando login com sessoes
+- Criação de tabela para gerenciamento de Usuários com permissões somente ao administrador
--- pagina-adm.php
+++ pagina-adm.php
@@ -1,6 +1,6 @@
 <?php
+
 session_start();
-include_once('header.php');
 if(!isset($_SESSION['email'])  == true and (!isset($_SESSION['senha']) == true )){
 
     unset($_SESSION['email']);
@@ -9,6 +9,7 @@ if(!isset($_SESSION['email'])  == true and (!isset($_SESSION['senha']) == true )
 
 }
 $logado = $_SESSION['email'];
+
 ?>
 
 <!DOCTYPE html>
@@ -64,7 +65,3 @@ $logado = $_SESSION['email'];
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-
-<?php
-include_once('footer.php');
-?>
