69ec642e8b03c60f8e1a6c3a49883b95a56cf5de
Lucas <lucaspicoli.leme@gmail.com>
2025-01-21 17:02:15 -0300

    Cria migrations que altera tabela comissao
    Alterado os campos da tabela comissoes para NOT NULL

---
+++ Migrations/008-alterando-campos-da-tabela-comissoes-para-not-null.php
@@ -0,0 +1,18 @@
+/*
+
+ALTER TABLE comissoes
+MODIFY valor_comissao INT NOT NULL;
+
+ALTER TABLE comissoes
+MODIFY dt_venda DATE NOT NULL;
+
+ALTER TABLE comissoes
+MODIFY carro_id INT NOT NULL;
+
+ALTER TABLE comissoes
+MODIFY usuario_id INT NOT NULL;
+
+ALTER TABLE comissoes
+MODIFY status VARCHAR(50) NOT NULL;
+
+*/
