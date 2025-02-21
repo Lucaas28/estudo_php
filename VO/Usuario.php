c71246719e54061251b2f6657e1889a4cf788906
Lucas <lucaspicoli.leme@gmail.com>
2025-02-21 17:14:03 -0300

    Cria pasta VO
    Movido arquivos VO para essa nova pasta

---
+++ VO/Usuario.php
@@ -0,0 +1,23 @@
+<?php
+include_once('config.php');
+require_once('BancoDeDados.php');
+
+class Usuario{
+
+    public $id;
+    public $nome;
+    public $email;
+    public $senha;
+    public $tipoDoUsuario;
+    public $comissao;
+
+    public function __construct($id, $nome, $email, $senha, $tipoDoUsuario, $comissao)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->senha = $senha;
+        $this->tipoDoUsuario = $tipoDoUsuario;
+        $this->comissao = $comissao;
+    }
+}
